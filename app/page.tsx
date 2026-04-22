4c10cb4efc39a3745059adea29847c74179de543
saswatmukerji008-sys <saswatmukerji008@gmail.com>
2026-04-22 08:09:18 +0530

    Create Home component for main page

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main style={{
+      minHeight: '100vh',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontFamily: 'sans-serif'
+    }}>
+      <h1>Welcome to DemandIQ</h1>
+      <p>Your demand planning platform</p>
+    </main>
+  )
+}
